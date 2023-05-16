c012c180077ee22357d884b445cf91f9a70b3d99
Leonardo Bordoni <bordonileonardo@gmail.com>
2023-05-16 18:50:54 +0200

    progetto inizio

---
+++ grp_15/laraProj6/app/Http/Controllers/PublicController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Catalog;
+use Illuminate\Support\Facades\Log;
+
+
+class PublicController extends Controller
+{
+    
+}
---
+++ grp_15/laraProj6/app/Http/Controllers/UserController.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class userController extends Controller {
+
+}
