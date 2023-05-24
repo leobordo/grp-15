905bacb74a18fe79f576852c710e090f1c2b4acd
Daniel-Diocis <danieldiocis@gmail.com>
2023-05-24 23:36:01 +0200

    c+

--- grp_15/laraProj6/resources/views/home.blade.php
+++ grp_15/laraProj6/resources/views/home.blade.php
@@ -3,24 +3,11 @@
 
 @section('title','home')
 @section('content')
-<<<<<<< Updated upstream
           <form class="CercaUtenti-form"  action={{ route('showRisultatiPromo') }} method='GET'>
             <input type="text" placeholder="Cerca promozioni" name='CercaPromo-input' class="CercaPromo-input">
             <button type="submit" class="CercaUtenti-bottone">Cerca</button>
           </form>
-         
-          
-         
-=======
-          <div class="search-bar">
-              <div>
-                  <input aria-label="search text" class="searchbar-input sc-ion-searchbar-md" inputmode="search" placeholder="Cerca su  my little coupony" type="search"
-                      autocomplete="off" autocorrect="off" wtx-context="14F1BC83-4A5A-444C-A0A9-BB910524D3AE" >
-              </div>
-              
-          </div>
->>>>>>> Stashed changes
-                 
+        
                 <h2>Promozioni</h2>
 
                 <div class="grid-container">
