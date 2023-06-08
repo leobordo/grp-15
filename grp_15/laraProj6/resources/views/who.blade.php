3c9b70775a288ad41bc874a4c822f7d7aa10aab6
sameonline <samueledisumma@gmail.com>
2023-06-08 22:49:04 +0200

    who un pò più bella

--- grp_15/laraProj6/resources/views/who.blade.php
+++ grp_15/laraProj6/resources/views/who.blade.php
@@ -3,19 +3,18 @@
 @section('title','Chi siamo')
 
 @section('content')
-        <br>
-        <br>
-        <h1>
-            Chi siamo
-        </h1>
-        <br>
-        <br>
-        <br>
-        <p>Contatti: Mario Rossi</p>
-        <br>
-        <p>E-mail: mario.rossi@google.com</p>
-        <br>
-        <p>Telefono: 3726389014</p>
+<div style="display: flex; flex-direction: column; align-items: center; text-align: center;">
+    <h1>Chi Siamo</h1>
+
+    <p>Benvenuti nel nostro sito di coupon! Siamo un team appassionato che cerca costantemente offerte e sconti per aiutarti a risparmiare sui tuoi acquisti online e offline. La nostra missione è fornire ai nostri utenti le migliori opportunità di risparmio su una vasta gamma di prodotti e servizi.</p>
+
+    <div class="contact-info" >
+        <h2>Contattaci</h2>
+        <p>Email: <a href="mailto:mariorossi@gmail.com">mariorossi@gmail.com</a></p>
+        <p>Telefono: 333-3898-976</p>
+    </div>
+
+</div>
         
 
         
